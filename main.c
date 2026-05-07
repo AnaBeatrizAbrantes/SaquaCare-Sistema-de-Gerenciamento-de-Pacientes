27c2f32d7cd115d05655a0cfac4d6e74a248969f
Nayara <nayaraemelly698@gmail.com>
2026-05-06 23:15:50 -0300

    hehe

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#ifndef HOSPITAL_H
+//#ifndef HOSPITAL_H
 #define HOSPITAL_H
 
 #define MAX 10
@@ -11,33 +11,3 @@ typedef struct {
     int tipo; // 1-Emergência, 2-Consulta, 3-Exame
 } Paciente;
 
-// Estruturas
-typedef struct {
-    Paciente itens[MAX];
-    int topo;
-} Pilha;
-
-typedef struct {
-    Paciente itens[MAX];
-    int inicio, fim;
-} Fila;
-
-typedef struct {
-    Paciente itens[MAX_CIRCULAR];
-    int inicio, fim, total;
-} FilaCircular;
-
-// Protótipos
-void initPilha(Pilha *p);
-void push(Pilha *p, Paciente pac);
-Paciente pop(Pilha *p);
-
-void initFila(Fila *f);
-void enqueue(Fila *f, Paciente pac);
-Paciente dequeue(Fila *f);
-
-void initCircular(FilaCircular *f);
-void enqueueCircular(FilaCircular *f, Paciente pac);
-Paciente dequeueCircular(FilaCircular *f);
-
-#endif
