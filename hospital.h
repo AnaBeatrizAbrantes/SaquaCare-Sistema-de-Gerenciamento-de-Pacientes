9a73e54fe766e78d6cc71ee041ffd69e584cd6d7
Nayara <nayaraemelly698@gmail.com>
2026-05-06 23:37:50 -0300

    ADICIONEI O .H E AS COISAS

--- hospital.h
+++ hospital.h
@@ -1,1 +1,14 @@
 // criei o .h
+
+#ifndef HOSPITAL_H
+#define HOSPITAL_H
+
+#define MAX 10
+#define MAX_CIRCULAR 8
+
+typedef struct {
+    char nome[50];
+    int idade;
+    int gravidade;
+    int tipo; // 1-Emergência, 2-Consulta, 3-Exame
+} Paciente;
--- main.c
+++ main.c
@@ -1,13 +1,1 @@
-//#ifndef HOSPITAL_H
-#define HOSPITAL_H
-
-#define MAX 10
-#define MAX_CIRCULAR 8
-
-typedef struct {
-    char nome[50];
-    int idade;
-    int gravidade;
-    int tipo; // 1-Emergência, 2-Consulta, 3-Exame
-} Paciente;
-
+//
